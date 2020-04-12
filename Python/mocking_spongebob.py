5ab454ebed8bdbebc0c894b2889234bfefc11952
Béla Durucz <mod4sw@gmail.com>
2020-04-12 12:09:15 +0200

    Mocking spongebob meme
    Added a new project: convert any text easily to mocking SpongeBob format

---
+++ Python/mocking_spongebob.py
@@ -0,0 +1,19 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Don\'t forget the text to convert!")
+    else:
+        spongebob_text = ""
+        capitalize = False
+        for word in sys.argv[1:]:
+            for c in word:
+                if c.isalpha() == True:
+                    if capitalize == True:
+                        c = c.upper()
+                    else:
+                        c = c.lower()
+                    capitalize = not capitalize
+                spongebob_text += c
+            spongebob_text += " "    
+        print(spongebob_text)
